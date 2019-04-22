c03af53e3ebf49a47b90a6458b971279d6c6a698
ksgebrekirstos98 <ksgebrekirstos98@gmail.com>
2019-04-22 17:15:16 -0400

    Tab progress Kaleb
    Might need to work on the constraints

--- app/src/main/java/com/example/GonkDroids/StudyU/MainActivity.java
+++ app/src/main/java/com/example/GonkDroids/StudyU/MainActivity.java
@@ -1,22 +1,46 @@
 package com.example.GonkDroids.StudyU;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private SectionsPageAdapter mSectionsPageAdapter;
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calander_main);
+        Log.d(TAG, "onCreate: Starting.");
+
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        //Set up the ViewPager with the sections adapter
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
     }
 
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CalendarViewFragment(), "Calendar");
+        adapter.addFragment(new AssignmentViewFragment(), "Assignments");
+        viewPager.setAdapter(adapter);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
